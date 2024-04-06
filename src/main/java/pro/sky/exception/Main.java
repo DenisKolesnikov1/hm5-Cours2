54a222ddb7fb3a6b08fa9e329c08870bbbe2050c
Денис Колесников <73371887@mail.ru>
2024-04-06 19:34:21 +0300

    homework5

--- src/main/java/pro/sky/exception/Main.java
+++ src/main/java/pro/sky/exception/Main.java
@@ -1,7 +1,19 @@
 package pro.sky.exception;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Homework 5");
+
+        try {
+            Validator.check("java_skypro_go94673$", "java_skypro_go94673", "java_skypro_go946734");
+            Validator.check("java_skypro_go94673", "java_skypro_go94673", "java_skypro_go946734");
+            Validator.check("java_skypro_go94673", "java_skypro_go94673", "java_skypro_go946734");
+            Validator.check("java_skypro_go94673", "java_skypro_go94673", "java_skypro_go946734");
+
+        } catch (WrongLoginException | WrongPasswordException exception) {
+            System.out.println("не верные входные данные:" + exception.getMessage());
+        }
+
     }
 }
