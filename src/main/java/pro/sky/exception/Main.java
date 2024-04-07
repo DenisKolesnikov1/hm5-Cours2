5ce22d8067bf919510ebc400cddc05aa3b3717a2
Денис Колесников <73371887@mail.ru>
2024-04-07 16:01:56 +0300

    homework5

--- src/main/java/pro/sky/exception/Main.java
+++ src/main/java/pro/sky/exception/Main.java
@@ -3,13 +3,22 @@ package pro.sky.exception;
 import java.util.Arrays;
 
 public class Main {
+
     public static void main(String[] args) {
 
         try {
-            Validator.check("java_skypro_go94673$", "java_skypro_go94673", "java_skypro_go946734");
-            Validator.check("java_skypro_go94673", "java_skypro_go94673", "java_skypro_go946734");
-            Validator.check("java_skypro_go94673", "java_skypro_go94673", "java_skypro_go946734");
-            Validator.check("java_skypro_go94673", "java_skypro_go94673", "java_skypro_go946734");
+             Validator.check("java_skypro_go94673$", "java_skypro_go94673", "java_skypro_go946734");
+            //(не допустимый символ):
+        } catch (WrongLoginException | WrongPasswordException exception) {
+            System.out.println("не верные входные данные:" + exception.getMessage());
+        }
+
+            //validator.check("java_skypro_go94673", "java_skypro_go94673", "java_skypro_go946734");
+            //validator.check("java_skypro_go94673", "java_skypro_go94673", "java_skypro_go946734");
+
+        try {
+             Validator.check("java_skypro_go94673", "java_skypro_go94673%", "java_skypro_go946734");
+             //(не корректно введены данные):
 
         } catch (WrongLoginException | WrongPasswordException exception) {
             System.out.println("не верные входные данные:" + exception.getMessage());
